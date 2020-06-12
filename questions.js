74d60c7a15a1dfb92e8f8974ba5dd2f20626d2a3
Caique <caiquedutraa@hotmail.com>
2020-06-12 11:41:04 -0300

    comittando

---
+++ questions.js
@@ -0,0 +1,12 @@
+var questions = [
+  ["\'Povo de tebas tendo chegado ao meu conhecimento que o rei Edipo fez acusações pesadas contra mim, eu venho aqui para fazer sentir a minha  mais total indignação\'. por quem foram ditas essas palavras?", "Creonte", "Tiresias", "Jocasta","None", "1"],
+  ["Onde Édipo nasceu?", "Corinto", "Tebas", " Delfos", "none","2"],
+  ["Após Édipo chegar ate a tebas e  decifrar o enigma, Qual é a sua recompensa?", "ganhar um grande valor em dinheiro", "ser rei e casar-se com a viúva", " poder desfrutar de tudo do reino", "nenhuma das respostas","1"],
+  ["Quando Édipo era jovem ele recebeu uma profecia. Qual era essa profecia?", " Matar um dos seus filhos", "  Matar um dos seus filhos", "Casar com sua mãe e  matar seu pai", "Morrer aos 30 anos", "Engravidar uma mulher fora do casamento?","1"],
+  ["Qual seria sua reação se você fosse Jocasta ?", "  Continuaria casada e manteria sua vida", " iria embora com Edipo ","Cometeria suicidio","Mataria Édipo","1"],
+  ["Se você fosse Sófocles qual final daria para Édipo?", "Manteria o final escolhido pelo autor", "Édipo continuaria sendo rei, mas tinha que pedir perdão para todo o povo", " Édipo continuaria morando em tebas/n mas apenas como um cidadão", "Cometer Suicidio","1"],
+  ["Qual dos Filhos de Édipo foi embora com ele?", "Antígona", "Ismênia e Polinice", "Etéocles", "Édipo não tinha filhos","1"],
+  ["Após descobrir que Jocasta e sua esposa, qual a reação de Édipo?", "Ignorar o passado e seguir feliz com sua familia", "Entrega o reino para um de seus filhos", "Vai embora de tebas com sua familia", " Fura os próprios olhos e vai embora com sua filha Jocasta da cidade","1"],
+  ["Depois de Édipo descobrir que si mesmo era assassino de laios ele se mata", "Verdadeiro", "Falso", "", "","1"],
+  [" Édipo acusa creonte  de traição", "Verdadeiro", "Falso", "", "","1"]
+  ];
