fc5af9f9a96fc4179157b963d3df41e4ae229352
debora99 <67063913+debora99@users.noreply.github.com>
2020-06-23 23:05:13 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ var questions = [
   ["\'Povo de tebas tendo chegado ao meu conhecimento que o rei Edipo fez acusações pesadas contra mim, eu venho aqui para fazer sentir a minha  mais total indignação\'. por quem foram ditas essas palavras?", "Creonte", "Tiresias", "Jocasta","None", "1"],
   ["Onde Édipo nasceu?", "Corinto", "Tebas", " Delfos", "none","2"],
   ["Após Édipo chegar ate a tebas e  decifrar o enigma, Qual é a sua recompensa?", "ganhar um grande valor em dinheiro", "ser rei e casar-se com a viúva", " poder desfrutar de tudo do reino", "nenhuma das respostas","1"],
-  ["Quando Édipo era jovem ele recebeu uma profecia. Qual era essa profecia?", " Matar um dos seus filhos", "  Matar um dos seus filhos", "Casar com sua mãe e  matar seu pai", "Morrer aos 30 anos", "Engravidar uma mulher fora do casamento?","1"],
+  ["Quando Édipo era jovem ele recebeu uma profecia. Qual era essa profecia?", " Matar um dos seus filhos", "  Matar todos os seus filhos", "Casar com sua mãe e  matar seu pai", "Morrer aos 30 anos", "Engravidar uma mulher fora do casamento?","1"],
   ["Qual seria sua reação se você fosse Jocasta ?", "  Continuaria casada e manteria sua vida", " iria embora com Edipo ","Cometeria suicidio","Mataria Édipo","1"],
   ["Se você fosse Sófocles qual final daria para Édipo?", "Manteria o final escolhido pelo autor", "Édipo continuaria sendo rei, mas tinha que pedir perdão para todo o povo", " Édipo continuaria morando em tebas/n mas apenas como um cidadão", "Cometer Suicidio","1"],
   ["Qual dos Filhos de Édipo foi embora com ele?", "Antígona", "Ismênia e Polinice", "Etéocles", "Édipo não tinha filhos","1"],
